d33e69e87816068fb65fb5b1436b8a56c712cdca
Ryo Sskai <r.sakai@aifulmf.com>
2025-06-22 21:16:40 +0900

    アクセスカウンター実装とテストコード

--- src/Models/ContentViewSummary.php
+++ src/Models/ContentViewSummary.php
@@ -2,6 +2,8 @@
 
 namespace Feeldee\Tracking\Models;
 
+use Carbon\Carbon;
+use Feeldee\Framework\Models\Content;
 use Feeldee\Framework\Models\Profile;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -38,5 +40,54 @@ class ContentViewSummary extends Model
     public function content()
     {
         return $this->morphTo();
+    }
+
+    /**
+     * プロフィールによる絞り込み条件のスコープ
+     * 
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param Profile $profile プロフィール
+     * @return void
+     */
+    public function scopeWhereProfile($query, Profile $profile): void
+    {
+        $query->where('profile_id', $profile->id);
+    }
+
+    /**
+     * コンテンツによる絞り込み条件のスコープ
+     * 
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param Content $content コンテンツ
+     * @return void
+     */
+    public function scopeWhereContent($query, Content $content): void
+    {
+        $query->where('content_id', $content->id)
+            ->where('content_type', $content->getMorphClass());
+    }
+
+    /**
+     * コンテンツ種別による絞り込み条件のスコープ
+     * 
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $content_type コンテンツ種別
+     * @return void
+     */
+    public function scopeWhereContentType($query, string $content_type): void
+    {
+        $query->where('content_type', $content_type);
+    }
+
+    /**
+     * コンテンツ閲覧日による絞り込み条件のスコープ
+     * 
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string|Carbon $viewed_date コンテンツ閲覧日（時刻が含まれる場合は、その日付のみを対象とする）
+     * @return void
+     */
+    public function scopeWhereViewedDate($query, string|Carbon $viewed_date): void
+    {
+        $query->whereDate('viewed_date', $viewed_date);
     }
 }
--- src/Services/ContentViewService.php
+++ src/Services/ContentViewService.php
@@ -4,10 +4,16 @@ namespace Feeldee\Tracking\Services;
 
 use Feeldee\Framework\Models\Content;
 use Carbon\Carbon;
+use Feeldee\Framework\Models\Profile;
 use Feeldee\Tracking\Models\ContentViewHistory;
+use Feeldee\Tracking\Models\ContentViewSummary;
 
 class ContentViewService
 {
+    const ALL = 'all';
+    const VIEWED_DATE_TODAY = 'today';
+    const VIEWED_DATE_YESTERDAY = 'yesterday';
+
     /**
      * コンテンツ閲覧履歴を登録します。
      * 
@@ -25,5 +31,48 @@ class ContentViewService
         }
 
         return false;
+    }
+
+    /**
+     * コンテンツ閲覧回数合計集計
+     * 
+     * プロフィール毎またはコンテンツ毎のコンテンツ閲覧回数合計を集計します。
+     * 
+     * @param Profile|Content $target 集計対象のプロフィールまたはコンテンツ
+     * @param string|Carbon $viewed_date コンテンツ閲覧日時絞り込み条件（all:全て|today:今日|yesterday:昨日|指定日時）
+     * @param string $content_type コンテンツ種別絞り込み条件（all:全て|コンテンツ種別）
+     * @return int コンテンツ閲覧回数合計
+     */
+    public function count(Profile|Content $target, string|Carbon $viewed_date = Self::ALL, string $content_type = Self::ALL): int
+    {
+        // 集計対象の型を確認し、クエリビルダを初期化
+        if ($target instanceof Profile) {
+            $query = ContentViewSummary::whereProfile($target);
+        } elseif ($target instanceof Content) {
+            $query = ContentViewSummary::whereContent($target);
+        } else {
+            return 0;
+        }
+        // コンテンツ閲覧日時絞り込み条件
+        if ($viewed_date !== self::ALL) {
+            if ($viewed_date === self::VIEWED_DATE_TODAY) {
+                $query->whereViewedDate(Carbon::today());
+            } elseif ($viewed_date === self::VIEWED_DATE_YESTERDAY) {
+                $query->whereViewedDate(Carbon::yesterday());
+            } else {
+                $query->whereViewedDate($viewed_date);
+            }
+        }
+        // コンテンツ種別絞り込み条件
+        if ($content_type !== self::ALL) {
+            $query->whereContentType($content_type);
+        }
+        // 取得したレコードのコンテンツ閲覧回数の合計を計算
+        $result = $query->get()
+            ->reduce(function ($carry, $item) {
+                return $carry + $item->view_count;
+            }, 0);
+        // 合計閲覧回数を返す
+        return $result;
     }
 }
